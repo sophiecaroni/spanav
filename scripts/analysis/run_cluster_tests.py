dbc868695d9fe1581dc49dcd738f9088ef48cd0a
sophiecaroni <sophie.caroni@gmail.com>
2026-04-28 09:24:03 +0200

    Export results of cluster-based tests
    - Refactor print_cluster_test_results into format_cluster_test_results
    - Export results in txt and pkl formats

--- scripts/analysis/run_cluster_tests.py
+++ scripts/analysis/run_cluster_tests.py
@@ -10,18 +10,25 @@
 """
 from spanav_tbi.processing.tfr import get_sid_level_tfr_df
 from spanav_tbi.processing.psd import get_sid_level_psd_df
-from spanav_tbi.analysis.cluster_tests import run_cluster_test_tfr, run_cluster_test_psd, format_cluster_test_results
+from spanav_tbi.analysis.cluster_tests import (
+    run_cluster_test_tfr,
+    run_cluster_test_psd,
+    format_cluster_test_results,
+    save_cluster_test_results,
+)
 
 
 def run_cluster_tests(
-    dev: bool = False,
-    verbose: bool = False,
-    **kwargs,
+        dev: bool = False,
+        verbose: bool = False,
+        save: bool = False,
+        **kwargs,
 ) -> None:
     """
     Run spectral cluster permutation tests on channel-averaged TFR and PSD data, separately per group.
     :param dev: bool, if True loads test data and reduces n_permutations to 10.
     :param verbose: bool, whether to print a summary of results per group.
+    :param save: bool, whether to save results to disk.
     :param kwargs: forwarded to run_cluster_test_tfr and run_cluster_test_psd (e.g. effects).
     """
     kwargs.setdefault('n_permutations', 10 if dev else 1000)
@@ -44,14 +51,18 @@ def run_cluster_tests(
             if verbose:
                 print(format_cluster_test_results(spct_obj, results, group, included_sids))
 
+            if save:
+                save_cluster_test_results(group, spct_obj, results, included_sids)
+
 
 if __name__ == '__main__':
-    TFR_EPO_TYPES = ['blMovOn_wide', 'blContMov_wide']
-    PSD_EPO_TYPES = ['blMovOn', 'blContMov']
-    FACTORS = ['cond']
     EFFECTS = ['A']  # main effect of the condition
+    TFR_EPO_TYPES = ['blObjPres_wide', 'blMovOn_wide', 'blContMov_wide']
+    PSD_EPO_TYPES = ['blObjPres', 'blMovOn', 'blContMov']
+    FACTORS = ['cond', 'epo_type']
 
     run_cluster_tests(
         dev=True,
         verbose=True,
+        save=True,
     )
