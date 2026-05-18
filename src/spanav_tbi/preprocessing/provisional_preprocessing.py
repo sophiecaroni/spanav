83411c4631be9674c5af80dc2f23bbba70cb3dcc
sophiecaroni <sophie.caroni@gmail.com>
2026-05-18 13:28:28 +0200

    Add bad channels interpolation to provisional preprocessing of one recording
    Because of particularly noisy channels

--- src/spanav_tbi/preprocessing/provisional_preprocessing.py
+++ src/spanav_tbi/preprocessing/provisional_preprocessing.py
@@ -4,7 +4,7 @@ import time
 from pathlib import Path
 
 
-def get_server_raw_path(subject: str, block: str, task: str) -> Path:
+def _get_server_raw_path(subject: str, block: str, task: str) -> Path:
     fname = f'sub-{subject}_ses-1_task-{task}_acq-{block}_eeg.vhdr'
     return io.get_raw_eeg_path(subject, server=True) / fname
 
@@ -14,9 +14,9 @@ def preproc_pipeline(subject: str, block: str, task: str = 'SpaNav', fast: bool 
     Load a raw BrainVision recording from the server, apply automated preprocessing,
     and save as .fif to the local preproc directory.
     """
-    raw_path = get_server_raw_path(subject, block, task)
-    print(f"\tLoading : {raw_path}")
-    raw = mne.io.read_raw_brainvision(raw_path, preload=False, verbose=verbose)
+    raw_path = _get_server_raw_path(subject, block, task)
+    print(f"\tLoading : {raw_path.stem}")
+    raw = mne.io.read_raw_brainvision(raw_path, preload=False, verbose=False)
     full_duration = raw.times[-1]
 
     # Crop first 145 s of recording in any case because they are encoding phase and are not needed at this stage
@@ -42,8 +42,13 @@ def preproc_pipeline(subject: str, block: str, task: str = 'SpaNav', fast: bool 
     else:
         print(f"\tLoad (full) : {full_duration:.0f} s of data in {load_elapsed:.1f} s")
 
+    # Automatic bad channels detection needed for this subject
+    raw.resample(sfreq=250, verbose=verbose)  # need resampling before it
+    if subject == '73A01' and block == 'block2':
+        raw.info['bads'] = ['C1', 'C2', 'C5', 'C6', 'CP1', 'CP2', 'CPz', 'F2', 'F7', 'F8', 'FC1', 'FC2', 'FC3', 'FC4', 'FC5', 'FC6', 'Fp1', 'Fpz', 'FT8', 'FT10', 'Oz', 'P2', 'PO4', 'PO7', 'Pz', 'T7', 'TP9']
+        raw.interpolate_bads(reset_bads=True)
+
     raw.filter(l_freq=1, h_freq=60, n_jobs=-1, verbose=verbose)
-    raw.resample(sfreq=250, verbose=verbose)
     raw.notch_filter(freqs=50.0, verbose=verbose)
     raw.set_eeg_reference(projection=False, verbose=verbose)
 
